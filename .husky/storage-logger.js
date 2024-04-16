dc16164f17348ce5b0f5d66add9cba9ab8c11f51
huyhuynh3103 <huy.huynh@skymavis.com>
2024-04-16 11:08:11 +0700

    chore: linter storagelayout.js

--- .husky/storage-logger.js
+++ .husky/storage-logger.js
@@ -3,59 +3,58 @@ const fileIn = process.argv[2];
 const fileOut = process.argv[3];
 
 if (!fileIn) {
-	console.error('Invalid input');
+  console.error('Invalid input');
 }
 
 fs.readFile(fileIn, 'utf8', (err, data) => {
-	console.log("fileIn", fileIn)
-	if (err) {
-		console.error('Error reading file:', err);
-		return;
-	}
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
 
-	try {
-		const jsonData = JSON.parse(data);
+  try {
+    const jsonData = JSON.parse(data);
 
-		console.log("jsonData", jsonData.storageLayout);
-		if (typeof jsonData.storageLayout == 'undefined') return;
-		if (jsonData.storageLayout.storage.length == 0) return;
+    if (typeof jsonData.storageLayout == 'undefined') return;
+    if (jsonData.storageLayout.storage.length == 0) return;
 
-		const hasAst = typeof jsonData.ast != 'undefined';
-		let storageLayout;
-		if (hasAst) {
-			const absolutePath = jsonData.ast.absolutePath;
-			if (typeof absolutePath == 'undefined') return;
-			if (!absolutePath.startsWith('src')) return;
-			storageLayout = jsonData.storageLayout.storage;
-		} else {
-			// filter only contracts in src/*
-			storageLayout = jsonData.storageLayout.storage.filter(({ contract }) =>
-				contract.startsWith('src')
-			);
-		}
+    const hasAst = typeof jsonData.ast != 'undefined';
 
-		const outputData = storageLayout
-			.map(({ contract, label, offset, slot, type: typeId }) => {
-				const typeObj = jsonData.storageLayout.types[typeId];
-				const typeLabel = typeObj.label;
-				const numberOfBytes = typeObj.numberOfBytes;
-				return `${contract}:${label} (storage_slot: ${slot}) (offset: ${offset}) (type: ${typeLabel}) (numberOfBytes: ${numberOfBytes})`;
-			})
-			.join('\n');
+    let storageLayout;
+    if (hasAst) {
+      const absolutePath = jsonData.ast.absolutePath;
+      if (typeof absolutePath == 'undefined') return;
+      if (!absolutePath.startsWith('src')) return;
+      storageLayout = jsonData.storageLayout.storage;
+    } else {
+      // filter only contracts in src/*
+      storageLayout = jsonData.storageLayout.storage.filter(({ contract }) =>
+        contract.startsWith('src')
+      );
+    }
 
-		if (outputData == '') return;
+    const outputData = storageLayout
+      .map(({ contract, label, offset, slot, type: typeId }) => {
+        const typeObj = jsonData.storageLayout.types[typeId];
+        const typeLabel = typeObj.label;
+        const numberOfBytes = typeObj.numberOfBytes;
+        return `${contract}:${label} (storage_slot: ${slot}) (offset: ${offset}) (type: ${typeLabel}) (numberOfBytes: ${numberOfBytes})`;
+      })
+      .join('\n');
 
-		if (!fileOut) {
-			console.log(outputData);
-		} else {
-			fs.writeFile(fileOut, outputData, 'utf-8', (err) => {
-				if (err) {
-					console.error('Error writing file:', err);
-					return;
-				}
-			});
-		}
-	} catch (err) {
-		console.error('Error parsing JSON:', err);
-	}
+    if (outputData == '') return;
+
+    if (!fileOut) {
+      console.log(outputData);
+    } else {
+      fs.writeFile(fileOut, outputData, 'utf-8', (err) => {
+        if (err) {
+          console.error('Error writing file:', err);
+          return;
+        }
+      });
+    }
+  } catch (err) {
+    console.error('Error parsing JSON:', err);
+  }
 });
