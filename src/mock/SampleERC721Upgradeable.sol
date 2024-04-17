c448189977b4623e09d327df8eced798fa806b99
huyhuynh3103 <huy.huynh@skymavis.com>
2024-04-17 10:25:42 +0700

    feat: move ci into another files

--- src/mock/SampleERC721Upgradeable.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-import "../upgradeable/ERC721CommonUpgradeable.sol";
-
-contract SampleERC721Upgradeable is ERC721CommonUpgradeable {}
