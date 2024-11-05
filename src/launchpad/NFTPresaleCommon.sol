314b7367efc1a9c549d47ab5819b9a4407a9a56e
huyhuynh3103 <huy.huynh@skymavis.com>
2024-11-05 10:10:58 +0700

    feat: support IERC165

--- src/launchpad/NFTPresaleCommon.sol
+++ src/launchpad/NFTPresaleCommon.sol
@@ -8,6 +8,6 @@ import { INFTPresale } from "../interfaces/launchpad/INFTPresale.sol";
 abstract contract NFTPresaleCommon is IERC165, INFTPresale {
   /// @dev Returns whether the contract supports the NFT presale interface.
   function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
-    return interfaceId == type(INFTPresale).interfaceId;
+    return interfaceId == type(INFTPresale).interfaceId || interfaceId == type(IERC165).interfaceId;
   }
 }
